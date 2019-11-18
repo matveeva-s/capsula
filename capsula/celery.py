dd2a109200dbc5db23a69088a983a6c38a4d60c5
Ivanova-Ekaterina <ivanova.ev@phystech.edu>
2019-11-18 19:18:15 +0300

    Добавлена загрузка картинки по ссылке и статус canceled

--- capsula/celery.py
+++ capsula/celery.py
@@ -4,3 +4,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capsula.settings.development')
 app = Celery('capsula')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
