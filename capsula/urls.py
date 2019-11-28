003ebe4ff10ca51b5d128cb8c0d138b32a0114a0
matveeva.s <matveeva.svetlana@gmail.com>
2019-11-28 15:49:55 +0300

    old search app removed again

--- capsula/urls.py
+++ capsula/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('user/',  include('user.urls')),
     path('library/', include('library.urls')),
     path('auth/', include('auth.urls')),
-    path('search/', include('elastic_app.urls')),
     path('auth/', include('auth.urls')),
     path('map/',  include('map.urls')),
     path('management/',  include('management.urls')),
